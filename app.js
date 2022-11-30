62a0d74e74652701eefe3390f0c9a00651585f9e
batrung2204 <114908362+batrung2204@users.noreply.github.com>
2022-11-30 15:42:26 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("get");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    formValidation();
+});
+
+let formValidation = () => {
+    if(textInput.value === ""){
+        console.log("failure");
+        msg.innerHTML = "Task cannot be blank";
+    } else {
+        console.log("success");
+        msg.innerHTML = "";
+        acceptData();
+        add.setAttribute("data-bs-dismiss", "modal");
+        add.click();
+
+        (() => {
+            add.setAttribute("data-bs-dismiss", "");
+        })();
+    }
+};
+
+let data = [{}];
+let acceptData = () => {
+    data.push({
+        text: textInput.value,
+        date: dateInput.value,
+        discription: textarea.value,
+    });
+    localStorage.setItem("data", JSON.stringify(data));
+    console.log(data);
+    createTasks();
+};
